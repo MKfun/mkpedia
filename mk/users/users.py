8795e877101364444a31668684f62100d6ae1bbd
TimurSennikov <timursennikov1@gmail.com>
2025-06-06 01:15:37 +0300

    v0.9

--- mk/users/users.py
+++ mk/users/users.py
@@ -5,6 +5,13 @@ from ..decorators import *
 
 user_bp = Blueprint("users", __name__, url_prefix="/users")
 
+def getcommitsform(count):
+    if count % 100 in (11, 12, 13, 14): return f"{count} правок"
+    if count % 10 == 1: return f"{count} правку"
+    if count % 10 in (2, 3, 4): return f"{count} правки"
+    if count == 0: return "правок... 0"
+    return f"{count} правок"
+
 def set_admin(v: bool):
     username = request.args.get("username")
 
@@ -38,7 +45,7 @@ def get_user():
 
     user = User.query.filter_by(username=username).first()
 
-    return render_template("users/user.html", user=user)
+    return render_template("users/user.html", user=user, formatted_commits=getcommitsform(user.commit_n))
 
 @user_bp.route("/promote")
 @admin_only
